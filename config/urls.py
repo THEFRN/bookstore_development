edddbfa65f28686f98db2f33b84262f76cdd28a4
skywalker <farhangian.ahmad@gmail.com>
2022-09-11 21:53:40 +0430

    fixed some bugs

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accoutns', include('django.contrib.auth.urls')),
-    path('accounts', include('accounts.urls')),
+    path('accoutns/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
