52b6cb85bd480c18d8d7a53c49a33bc015790ce2
skywalker <farhangian.ahmad@gmail.com>
2022-09-13 21:19:02 +0430

    plugged django crispy forms into the project

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
+    'crispy_forms',
+
     # Local Apps
     'accounts',
     'pages',
@@ -124,7 +128,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Authentication settings
 AUTH_USER_MODEL = 'accounts.CustomUser'
-
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# Crispy forms settings
+CRISPY_TEMPLATE_TAG = 'bootstrap4'
