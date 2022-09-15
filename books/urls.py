4860daa2469f8faa67424161242cb885013a3396
skywalker <farhangian.ahmad@gmail.com>
2022-09-15 19:47:35 +0430

    created a detail view for books

--- books/urls.py
+++ books/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.BookListview.as_view(), name='books_list')
+    path('', views.BookListview.as_view(), name='books_list'),
+    path('<int:pk>/', views.BookDetailView.as_view(), name='book_detail')
 ]
